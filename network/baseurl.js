8ff7623e7888e9352b12d0d4eb993bd8ab78768c
十 <welllon710>
2020-12-15 23:37:44 +0800

    last

--- network/baseurl.js
+++ network/baseurl.js
@@ -1,5 +1,5 @@
 
- //  const BaseUrl = 'http://42.192.92.208:90/api'
-   const BaseUrl = 'http://127.0.0.1:8000/api'
+   const BaseUrl = 'http://42.192.92.208:90/api'
+ //  const BaseUrl = 'http://127.0.0.1:8000/api'
  
 export default BaseUrl
